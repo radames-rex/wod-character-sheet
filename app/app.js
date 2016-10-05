caf369797716d6ecf240572fe3de478f22567ac7
Rádames Rex <Rdames Rex>
2016-10-04 23:54:21 -0300

    novas configurações do material

--- app/app.js
+++ app/app.js
@@ -1,25 +1,24 @@
 'use strict';
 
-angular.module('wodApp',[
-  'ngRoute',
-  'ngMaterial'
-])
-angular.module('wodApp').config(function($routeProvider){
-  $routeProvider
-    .when('/vampire',{
-      templateUrl: 'components/sheets/vampire.html',
-      controller: 'vampireCtrl'
-    })
-    .when('/wolf',{
-      templateUrl: 'components/sheets/werewolf.html',
-      controller: 'werewolfCtrl'
-    })
-    .otherwise({
-      redirectTo: '/vampire'
-    })
-})
-angular.module('wodApp').controller('worldCtrl', function($rootScope, $scope){
-  $scope.sheets = [];
-  $rootScope.sheets = [];
-  $scope.sheets = $rootScope.sheets;
-})
+  angular.module('wodApp',[
+    'ngRoute',
+    'ngMaterial'
+  ]).config(function($routeProvider){
+    $routeProvider
+      .when('/vampire',{
+        templateUrl: 'components/sheets/vampire.html',
+        controller: 'vampireCtrl'
+      })
+      .when('/wolf',{
+        templateUrl: 'components/sheets/werewolf.html',
+        controller: 'werewolfCtrl'
+      })
+      .otherwise({
+        redirectTo: '/vampire'
+      })
+  })
+  angular.module('wodApp').controller('worldCtrl', function($rootScope, $scope){
+    $scope.sheets = [];
+    $rootScope.sheets = [];
+    $scope.sheets = $rootScope.sheets;
+  })
